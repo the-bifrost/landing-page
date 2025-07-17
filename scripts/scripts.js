24ba6b66d39d2ec40a89e92d4b7d1884d7361561
kerlonr <mitokerlon1227@gmail.com>
2025-07-17 14:38:54 -0300

    Cellphone Scroll Speed changed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 ScrollSmoother.create({
   smooth: 1.5, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
-  smoothTouch: 0.5, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 });
 // Funções de animação GSAP
 
