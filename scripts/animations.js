6202881f870c486b6d38cbe24c8b514890858b0b
kerlonr <mitokerlon1227@gmail.com>
2025-07-17 10:34:04 -0300

    adicionar glow no card protocol, e reflexo no btn card

--- scripts/animations.js
+++ scripts/animations.js
@@ -243,3 +243,14 @@ function animateTimelineProgress() {
     },
   });
 }
+
+//TESTE GLOW
+document.querySelectorAll(".card__protocol").forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = ((e.clientX - rect.left) / rect.width) * 50;
+    const y = ((e.clientY - rect.top) / rect.height) * 50;
+    card.style.setProperty("--mouse-x", `${x}%`);
+    card.style.setProperty("--mouse-y", `${y}%`);
+  });
+});
